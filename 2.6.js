fa070c4e34452e9c60731838418dff10c7a21b38
Fanny Fraiture <ffraiture.fanny@gmail.com>
2021-09-24 15:35:15 +0200

    update js

---
+++ 2.6.js
@@ -0,0 +1,26 @@
+const readlineSync = require("readline-sync");
+const number = parseInt(readlineSync.question("Pick a number between 1 and 7"));
+
+switch (number) {
+  case 1:
+    console.log("Monday");
+    break;
+  case 2:
+    console.log("Tuesday");
+    break;
+  case 3:
+    console.log("Wednesday");
+    break;
+  case 4:
+    console.log("Thursday");
+    break;
+  case 5:
+    console.log("Friday");
+    break;
+  case 6:
+    console.log("Saturday");
+    break;
+  case 7:
+    console.log("Sunday");
+    break;
+}
